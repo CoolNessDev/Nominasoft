dd8f5ac2274198e7872f43e96cb39c908cdf5f26
Italo Tommy <71053391+ItaloTommy@users.noreply.github.com>
2021-06-02 12:27:10 -0500

    Add files via upload

--- Capa.4_Persistencia/FabricaDatos/FabricaAbstracta.cs
+++ Capa.4_Persistencia/FabricaDatos/FabricaAbstracta.cs
@@ -12,17 +12,11 @@ namespace Capa._4_Persistencia.FabricaDatos
         public static FabricaAbstracta crearInstancia()
         {
             return new FabricaSQLServer();
-            //return new FabricaMySQL();
         }
-
-        public abstract IAfp crearAfp(IGestorAccesoDatos gestorAccesoDatos);
-        public abstract IContrato crearContrato(IGestorAccesoDatos gestorAccesoDatos);
-        public abstract IDescuento crearDescuento(IGestorAccesoDatos gestorAccesoDatos);
-        public abstract IEmpleado crearEmpleado(IGestorAccesoDatos gestorAccesoDatos);
-        public abstract IIngreso crearIngreso(IGestorAccesoDatos gestorAccesoDatos);
-        public abstract IPago crearPago(IGestorAccesoDatos gestorAccesoDatos);
-        public abstract IPeriodo crearPeriodo(IGestorAccesoDatos gestorAccesoDatos);
-        public abstract IRol crearRol(IGestorAccesoDatos gestorAccesoDatos);
+        public abstract IGestorAccesoDatos crearGestorAccesoDatos();
+        public abstract IAfp crearAfpDAO(IGestorAccesoDatos gestorAccesoDatos);
+        public abstract IContrato crearContratoDAO(IGestorAccesoDatos gestorAccesoDatos);
+        public abstract IEmpleado crearEmpleadoDAO(IGestorAccesoDatos gestorAccesoDatos);
 
     }
 }
