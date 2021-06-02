3b42bf9aa5e8a06bf929bf3f550026f2b243146d
Italo Tommy <71053391+ItaloTommy@users.noreply.github.com>
2021-06-02 12:20:07 -0500

    Add files via upload

--- Capa.3_Dominio/Contratos/IEmpleado.cs
+++ Capa.3_Dominio/Contratos/IEmpleado.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Capa._3_Dominio.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace Capa._3_Dominio.Contratos
 {
     public interface IEmpleado
     {
-
+        Empleado BuscarEmpleadoPorDNI(int DNI);
     }
 }
